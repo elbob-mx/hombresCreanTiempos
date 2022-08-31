dc7c2fd8be6a872c1ffab4a3af25d6d6b08bf09e
elbob-mx <rob7th@gmail.com>
2022-08-30 23:53:28 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      small: '0px',
+      mobile: '360px',
+      tablet: '768px',
+      laptop: '976px',
+      desktop: '1440px',
+    },
+    extend: {
+      colors : {
+      },
+      fontFamily: {
+        'archivo': 'Archivo Narrow, sans-serif',
+        'lexend': 'Lexend Exa, sans-serif',        
+        },
+      fontWeight: {
+        hairline: 100,
+        'extra-light': 100,
+        thin: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        'extra-bold': 800,
+        black: 900,
+        }
+    },
+  },
+  plugins: [],
+}
