0f32092512d1560b705b7d14954d010f45400484
elbob-mx <rob7th@gmail.com>
2022-09-15 12:42:44 -0500

    js

--- controllers/change-button.js
+++ controllers/change-button.js
@@ -1,12 +1,25 @@
+const upQuotes = [
+  'tiempos difíciles',
+  'buenos tiempos',
+];
+
+const downQuotes = [
+  'personas fuertes',
+  'personas débiles',
+];
+
 function change() {
   // alert("change");
   var btn = document.getElementById("btn");
   var result = document.getElementById("result");
-  btn.innerHTML = "tiempos difíciles";
-  if (btn.innerHTML == "tiempos difíciles") {
-    result.innerHTML = "personas fuertes";
+  btn.innerHTML = upQuotes[0];
+  if (btn.innerHTML == upQuotes[0]) {
+    result.innerHTML = downQuotes[0];
   };
   if (btn.value == "debiles") {
     btn.value = "dificiles";
   };
+  console.log('función procesada');
+  console.log(upQuotes[0]);
+  console.log(downQuotes[0]);
 };
