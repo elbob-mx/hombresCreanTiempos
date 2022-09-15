2fade250ea3a5195cf9a2e2b456830745947c41e
elbob-mx <rob7th@gmail.com>
2022-09-15 13:53:20 -0500

    video src

--- controllers/change-button.js
+++ controllers/change-button.js
@@ -1,25 +1,42 @@
 const upQuotes = [
   'tiempos difíciles',
   'buenos tiempos',
+  'personas fuertes',
+  'personas débiles',
 ];
 
 const downQuotes = [
   'personas fuertes',
   'personas débiles',
+  'buenos tiempos',
+  'tiempos difíciles',
 ];
 
 function change() {
-  // alert("change");
   var btn = document.getElementById("btn");
   var result = document.getElementById("result");
+  var bgVideo = document.getElementById("bg-video");
+  
   btn.innerHTML = upQuotes[0];
+
   if (btn.innerHTML == upQuotes[0]) {
     result.innerHTML = downQuotes[0];
   };
+
   if (btn.value == "debiles") {
     btn.value = "dificiles";
   };
+
   console.log('función procesada');
-  console.log(upQuotes[0]);
-  console.log(downQuotes[0]);
+  console.log("frase: " + " " + upQuotes[0]);
+  console.log("frase: " + " " + downQuotes[0]);
+  console.log(bgVideo.src);
 };
+
+/*
+while (contador 'btn' < 3) {
+  contador 'btn' ++ ;
+  upQuotes[0++];
+  downQuotes[0++];
+}
+*/
