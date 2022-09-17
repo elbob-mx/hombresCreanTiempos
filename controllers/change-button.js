94ea864607a2037b824a29d3f788493f64f8b61a
elbob-mx <rob7th@gmail.com>
2022-09-17 00:35:44 -0500

    V0.9

--- controllers/change-button.js
+++ controllers/change-button.js
@@ -17,23 +17,35 @@ var result = document.getElementById("result");
 var bgVideo = document.getElementById("bg-video");
 
 function change() {
-  
-  btn.innerHTML = upQuotes[0];
-  result.innerHTML = downQuotes[0];
 
   if (btn.innerHTML == 'personas débiles', result.innerHTML == 'tiempos difíciles') {
-    } else if (btn.innerHTML == 'tiempos difíciles', result.innerHTML = 'personas fuertes') {
+    bgVideo.setAttribute('src', 'video/good times-shade.mp4')
+    btn.innerHTML = 'tiempos difíciles';
+    result.innerHTML = 'personas fuertes';
+    } else if (btn.innerHTML == 'tiempos difíciles', result.innerHTML == 'personas fuertes') {
+      btn.innerHTML = 'personas fuertes';
+      result.innerHTML = 'buenos tiempos';
+      bgVideo.setAttribute('src', 'video/bucha.mp4');
+    } else if (btn.innerHTML == 'personas fuertes', result.innerHTML == 'buenos tiempos') {
+      btn.innerHTML = 'buenos tiempos';
+      result.innerHTML = 'personas débiles';
       bgVideo.setAttribute('src', 'video/good times-shade.mp4');
-      }
+    } else if (btn.innerHTML == 'buenos tiempos', result.innerHTML == 'personas débiles') {
+      btn.innerHTML = 'personas débiles';
+      result.innerHTML = 'tiempos difíciles';
+      bgVideo.setAttribute('src', 'video/bucha.mp4');
+    }
 
-  if (btn.value == "debiles") {
-    btn.value = "dificiles";
+    if (btn.value == "debiles") {
+      btn.value = "dificiles";
+    };
+    
+    console.log('función procesada');
+    console.log("frase: " + " " + upQuotes[0]);
+    console.log("frase: " + " " + downQuotes[0]);
+    console.log(bgVideo.src);
+    console.log(btn.value);  
+    console.log(result.value);
+
   };
-  
-  console.log('función procesada');
-  console.log("frase: " + " " + upQuotes[0]);
-  console.log("frase: " + " " + downQuotes[0]);
-  console.log(bgVideo.src);
-  console.log(btn.value);  
-  console.log(result.value);
-};
+
