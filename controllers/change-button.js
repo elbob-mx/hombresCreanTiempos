758fa18c9eca4585aba9defb07f32d9e9adc5622
elbob-mx <rob7th@gmail.com>
2022-09-18 16:38:43 -0500

    classList font color

--- controllers/change-button.js
+++ controllers/change-button.js
@@ -22,18 +22,38 @@ function change() {
     bgVideo.setAttribute('src', 'video/good times-shade.mp4')
     btn.innerHTML = 'tiempos difíciles';
     result.innerHTML = 'personas fuertes';
+    document.getElementById("btn").classList.remove('text-black');
+    document.getElementById("btn").classList.add('text-white');
+    document.getElementById("result").classList.remove('text-black');
+    document.getElementById("result").classList.add('text-white');
+
     } else if (btn.innerHTML == 'tiempos difíciles', result.innerHTML == 'personas fuertes') {
       btn.innerHTML = 'personas fuertes';
       result.innerHTML = 'buenos tiempos';
       bgVideo.setAttribute('src', 'video/bucha.mp4');
+      document.getElementById("btn").classList.remove('text-white');
+      document.getElementById("btn").classList.add('text-black');
+      document.getElementById("result").classList.remove('text-white');
+      document.getElementById("result").classList.add('text-black');
+
     } else if (btn.innerHTML == 'personas fuertes', result.innerHTML == 'buenos tiempos') {
       btn.innerHTML = 'buenos tiempos';
       result.innerHTML = 'personas débiles';
       bgVideo.setAttribute('src', 'video/good times-shade.mp4');
+      btn.setAttribute('text-black', 'text-white');
+      document.getElementById("btn").classList.remove('text-black');
+      document.getElementById("btn").classList.add('text-white');
+      document.getElementById("result").classList.remove('text-black');
+      document.getElementById("result").classList.add('text-white');
+
     } else if (btn.innerHTML == 'buenos tiempos', result.innerHTML == 'personas débiles') {
       btn.innerHTML = 'personas débiles';
       result.innerHTML = 'tiempos difíciles';
       bgVideo.setAttribute('src', 'video/bucha.mp4');
+      document.getElementById("btn").classList.remove('text-white');
+      document.getElementById("btn").classList.add('text-black');
+      document.getElementById("result").classList.remove('text-white');
+      document.getElementById("result").classList.add('text-black');
     }
 
     if (btn.value == "debiles") {
