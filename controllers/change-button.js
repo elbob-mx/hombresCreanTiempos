3417229e8de6dd26656d8e51af5a451578671d03
elbob-mx <rob7th@gmail.com>
2022-09-19 15:39:55 -0500

    update fonts videos

--- controllers/change-button.js
+++ controllers/change-button.js
@@ -1,68 +1,44 @@
-const upQuotes = [
-  'tiempos difíciles',
-  'buenos tiempos',
-  'personas fuertes',
-  'personas débiles',
-];
-
-const downQuotes = [
-  'personas fuertes',
-  'personas débiles',
-  'buenos tiempos',
-  'tiempos difíciles',
-];
-
-var btn = document.getElementById("btn");
-var result = document.getElementById("result");
-var bgVideo = document.getElementById("bg-video");
+let btn = document.getElementById("btn");
+let result = document.getElementById("result");
+let bgVideo = document.getElementById("bg-video");
 
 function change() {
 
   if (btn.innerHTML == 'personas débiles', result.innerHTML == 'tiempos difíciles') {
-    bgVideo.setAttribute('src', 'video/good times-shade.mp4')
     btn.innerHTML = 'tiempos difíciles';
     result.innerHTML = 'personas fuertes';
-    document.getElementById("btn").classList.remove('text-black');
-    document.getElementById("btn").classList.add('text-white');
-    document.getElementById("result").classList.remove('text-black');
-    document.getElementById("result").classList.add('text-white');
+    document.getElementById("bg-video").classList.remove('scale-x-flip');
+    bgVideo.setAttribute('src', 'video/bucha.mp4')
 
     } else if (btn.innerHTML == 'tiempos difíciles', result.innerHTML == 'personas fuertes') {
       btn.innerHTML = 'personas fuertes';
       result.innerHTML = 'buenos tiempos';
-      bgVideo.setAttribute('src', 'video/bucha.mp4');
-      document.getElementById("btn").classList.remove('text-white');
-      document.getElementById("btn").classList.add('text-black');
-      document.getElementById("result").classList.remove('text-white');
-      document.getElementById("result").classList.add('text-black');
-
-    } else if (btn.innerHTML == 'personas fuertes', result.innerHTML == 'buenos tiempos') {
-      btn.innerHTML = 'buenos tiempos';
-      result.innerHTML = 'personas débiles';
-      bgVideo.setAttribute('src', 'video/good times-shade.mp4');
-      btn.setAttribute('text-black', 'text-white');
+      document.getElementById("bg-video").classList.add('scale-x-flip');
+      bgVideo.setAttribute('src', 'video/Women at work WWII 1943.mp4');
       document.getElementById("btn").classList.remove('text-black');
       document.getElementById("btn").classList.add('text-white');
       document.getElementById("result").classList.remove('text-black');
       document.getElementById("result").classList.add('text-white');
 
+    } else if (btn.innerHTML == 'personas fuertes', result.innerHTML == 'buenos tiempos') {
+      btn.innerHTML = 'buenos tiempos';
+      result.innerHTML = 'personas débiles';
+      // document.getElementById("bg-video").classList.add('scale-x-flip');
+      bgVideo.setAttribute('src', 'video/good times-shade.mp4');
+      btn.setAttribute('text-black', 'text-white');
+
     } else if (btn.innerHTML == 'buenos tiempos', result.innerHTML == 'personas débiles') {
       btn.innerHTML = 'personas débiles';
       result.innerHTML = 'tiempos difíciles';
+      // document.getElementById("bg-video").classList.add('scale-x-flip');
       bgVideo.setAttribute('src', 'video/Putin Laughing.mp4');
-      document.getElementById("bg-video").classList.add('scale-x-flip');
+      document.getElementById("btn").classList.remove('text-white');
+      document.getElementById("btn").classList.add('text-black');
+      document.getElementById("result").classList.remove('text-white');
+      document.getElementById("result").classList.add('text-black');
+      
     }
-
-    if (btn.value == "debiles") {
-      btn.value = "dificiles";
-    };
     
     console.log('función procesada');
-    console.log("frase: " + " " + upQuotes[0]);
-    console.log("frase: " + " " + downQuotes[0]);
-    console.log(bgVideo.src);
-    console.log(btn.value);  
-    console.log(result.value);
-
   };
 
